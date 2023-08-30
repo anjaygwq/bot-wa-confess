f25980888096f03f0186d161f4f8212c88de3092
anjaygwq <143594124+anjaygwq@users.noreply.github.com>
2023-08-30 16:17:27 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.fotomenfess = fs.readFileSync("./menfess.jpg")
-global.owner = ["62887435047326"]
+global.owner = ["6285808936396"]
 
 global.help = {
 Menu(pushname){
